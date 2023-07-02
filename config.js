9b490aca68e640b2975df53e1efe76589c57082a
Andowa <49613758+Andowa@users.noreply.github.com>
2023-07-02 11:07:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'EYPA Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793530271-bb6bb167cddea50377421917',
+    'm794644594-388e48d963ad781a3b0ee8d3',
+    'm793530289-f63b07b385f94ed09c0e03ee',
+    'm793530280-55a1604df16d0f36f18e1d1b',
+    'm794644549-6bff5605263d14916d5fa38c',
+    'm794644582-15efc72ee5d45144b98d40a9',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Status',
+      url: 'https://status.eqmemory.cn'
+    },
+  ],
+};
