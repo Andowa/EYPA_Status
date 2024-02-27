47e0097b7ff43ff8e51bf152fa331450e88725af
Andowa <49613758+Andowa@users.noreply.github.com>
2024-02-28 00:09:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm793530271-bb6bb167cddea50377421917',
-    'm794644594-388e48d963ad781a3b0ee8d3',
     'm793530280-55a1604df16d0f36f18e1d1b',
     'm794644549-6bff5605263d14916d5fa38c',
     'm794889307-c4af926af740479be4f8fe0e',
